72eba4d68de6ae16e9b02df0b92a39bd23b48681
jimmyatv <rasic.alexandar@gmail.com>
2023-03-17 15:59:37 +0100

    Responsive part2 and burger menu finished

--- app.js
+++ app.js
@@ -26,6 +26,18 @@ buttons.forEach(button => {
 
 const toTop = document.querySelector('.to-top');
 const headerNav = document.querySelector('.headerNav');
+const burger = document.querySelector('.burger');
+const mobileNav = document.querySelector('.mobile-nav');
+
+
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('is-active');
+  mobileNav.classList.toggle('is-active');
+});
+
+
+
 
 window.addEventListener('scroll', () => {
   if(window.pageYOffset > 100) {
